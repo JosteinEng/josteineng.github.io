2d65942cd4ca422ee3e89b3ae7da7a0993f583d1
Henning <henningens@hotmail.com>
2021-12-04 20:21:58 +0100

    Lagt til script

---
+++ script/gamerNoApi.js
@@ -0,0 +1,27 @@
+const requestUrl = "https://www.gamer.no/api/paradise/club/101211/matches/results"
+
+fetch(requestUrl)
+    .then(response => response.json())
+    .then(data => { 
+        const matchesEl = document.getElementById("matches")
+        matches = data.data
+        Object.values(matches).forEach(match => {
+            var matchDiv = document.createElement("div")
+            matchDiv.innerHTML = match.competition.name + " " + match.round_identifier_text
+
+            var resultDiv = document.createElement("div")
+            resultDiv.innerHTML = match.club_side == "home" ?
+                //Hvis hjemme, BRGN først
+                match.home_signup.team.name +
+                " " + match.home_score + " - " + match.away_score + " " +
+                match.away_signup.team.name
+                
+                //Hvis borte, BRGN først
+                : match.away_signup.team.name +
+                " " + match.away_score + " - " + match.home_score + " " +
+                match.home_signup.team.name
+            matchDiv.appendChild(resultDiv)
+
+            matchesEl.appendChild(matchDiv)
+        })
+    })
